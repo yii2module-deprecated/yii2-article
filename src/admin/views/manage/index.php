6e8218a727f809f495b97102704f7752063e9d62
Vitaliy <theyamshikov@yandex.ru>
2017-10-08 12:39:06 +0600

    refacto view

--- src/admin/views/manage/index.php
+++ src/admin/views/manage/index.php
@@ -2,7 +2,10 @@
 
 /* @var $this yii\web\View */
 
-use yii2lab\helpers\Page;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii2lab\misc\yii\grid\ActionColumn;
+use yii2lab\misc\yii\grid\TitleColumn;
 
 $this->title = t('article/main', 'list');
 
@@ -10,15 +13,24 @@ $baseUrl = $this->context->getBaseUrl();
 
 $columns = [
 	[
-		'attribute' => 'title',
-		'label' => t('main', 'title'),
+		'class' => TitleColumn::className(),
+		'baseUrl' => $baseUrl,
 	],
 	[
 		'attribute' => 'name',
 		'label' => t('main', 'name'),
 	],
+	[
+		'class' => ActionColumn::className(),
+	],
 ];
 
 ?>
 
-<?= Page::snippet('list', '@common', compact('dataProvider', 'baseUrl', 'columns')) ?>
+<?= GridView::widget([
+	'dataProvider' => $dataProvider,
+	'layout' => '{summary}{items}',
+	'columns' => $columns,
+]); ?>
+
+<?= Html::a(t('action', 'CREATE'), $baseUrl . 'create', ['class' => 'btn btn-success']) ?>
