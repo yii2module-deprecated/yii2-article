93bc98de5974da3bdf6dc01d59933928d17f1e21
Vitaliy <theyamshikov@yandex.ru>
2017-12-24 01:30:36 +0600

    make menu

---
+++ src/admin/helpers/Menu.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace yii2module\article\admin\helpers;
+
+// todo: отрефакторить - сделать нормальный интерфейс и родителя
+
+use common\enums\rbac\PermissionEnum;
+
+class Menu {
+	
+	static function getMenu() {
+		return [
+			'label' => ['article/main', 'title'],
+			'url' => 'article/manage',
+			'icon' => 'square-o',
+			'module' => 'article',
+			'access' => PermissionEnum::ARTICLE_POST_MANAGE,
+		];
+	}
+
+}
