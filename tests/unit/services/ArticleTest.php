aa369f7858ab4d89ef24ce2e3e2b978c8e43772d
Vitaliy <theyamshikov@yandex.ru>
2018-01-30 16:59:32 +0600

    make test case

--- tests/unit/services/ArticleTest.php
+++ tests/unit/services/ArticleTest.php
@@ -5,11 +5,14 @@ namespace tests\unit\services;
 use Codeception\Test\Unit;
 use UnitTester;
 use Yii;
+use yii\web\ForbiddenHttpException;
 use yii2lab\domain\BaseEntity;
 use yii2lab\domain\data\Query;
+use yii2module\article\domain\entities\ArticleEntity;
 use yii2module\article\domain\fixtures\ArticleCategoriesFixture;
 use yii2module\article\domain\fixtures\ArticleCategoryFixture;
 use yii2module\article\domain\fixtures\ArticleFixture;
+use yii2woop\account\domain\helpers\TestAuthHelper;
 
 /**
  * Class ArticleTest
@@ -20,6 +23,8 @@ use yii2module\article\domain\fixtures\ArticleFixture;
  */
 class ArticleTest extends Unit
 {
+	
+	const ADMIN_ID = 381949;
 	
 	public function _before()
     {
@@ -90,5 +95,56 @@ class ArticleTest extends Unit
 				],
 			],
 		], $entity);
+	}
+	
+	public function testInsert()
+	{
+		TestAuthHelper::authById(self::ADMIN_ID);
+		
+		$entity = Yii::$app->article->factory->entity->create(ArticleEntity::class);
+		$entity->name = 'services';
+		$entity->title = 'Our services';
+		$entity->content = 'Our services content';
+		Yii::$app->article->article->create($entity);
+		
+		$this->tester->assertEntity([
+			//'id' => 4,
+			'name' => 'services',
+			'title' => 'Our services',
+			'content' => 'Our services content',
+		], $entity);
+	}
+	
+	public function testUpdate()
+	{
+		TestAuthHelper::authById(self::ADMIN_ID);
+		
+		$entity = Yii::$app->article->article->oneById(1);
+		$entity->name = '111';
+		$entity->title = '222';
+		$entity->content = '333';
+		Yii::$app->article->article->updateById(1, $entity);
+		
+		$entity = Yii::$app->article->article->oneById(1);
+		
+		$this->tester->assertEntity([
+			//'id' => 4,
+			'name' => '111',
+			'title' => '222',
+			'content' => '333',
+		], $entity);
+	}
+	
+	public function testDelete()
+	{
+		TestAuthHelper::authById(self::ADMIN_ID);
+		
+		try {
+			Yii::$app->article->article->deleteById(1);
+			$this->tester->assertTrue(false);
+		} catch(ForbiddenHttpException $e) {
+			$this->tester->assertTrue(true);
+		}
+		Yii::$app->article->article->oneById(1);
 	}
 }
