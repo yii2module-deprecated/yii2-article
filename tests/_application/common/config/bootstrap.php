2c5af2c920540023fe8ba27c11df3ad90763b9f0
Vitaliy <theyamshikov@yandex.ru>
2018-03-01 21:30:51 +0600

    make functional test

--- tests/_application/common/config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use yii2lab\helpers\yii\FileHelper;
-
-$testDir = FileHelper::up(__DIR__, 3);
-
-Yii::setAlias('@tests', $testDir);
--- tests/_application/common/config/env.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use yii2lab\test\helpers\TestHelper;
-
-return TestHelper::loadEnvFromPath(__DIR__);
