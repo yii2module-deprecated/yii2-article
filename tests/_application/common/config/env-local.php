c34f3caf46634dc010d927ea1c019ca738cc1f77
Vitaliy <theyamshikov@yandex.ru>
2018-07-27 20:53:53 +0600

    fix test

--- tests/_application/common/config/env-local.php
+++ tests/_application/common/config/env-local.php
@@ -4,7 +4,14 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-
+	'servers' => [
+		'db' => [
+			'test' => [
+				'driver' => 'sqlite',
+				'dbname' => TestHelper::PACKAGE_TEST_DB_FILE,
+			],
+		],
+	],
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/env-local.php');
