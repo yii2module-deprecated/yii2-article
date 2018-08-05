99ba89b1b42cfb6d5ff105d00382d980dc56b8ab
Vitaliy <theyamshikov@yandex.ru>
2018-08-05 22:52:30 +0600

    fix test

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -4,7 +4,17 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-	'rbac' => 'yii2lab\rbac\domain\Domain',
+    'rbac' => [
+        'class' => 'yii2lab\rbac\domain\Domain',
+        'repositories' => [
+            'rule' => [
+                'ruleFile' => '@vendor/yii2lab/yii2-test/src/base/_application/common/data/rbac/rules.php',
+            ],
+            'item' => [
+                'itemFile' => '@vendor/yii2lab/yii2-test/src/base/_application/common/data/rbac/items.php',
+            ],
+        ],
+    ],
 	'article' => 'yii2module\article\domain\Domain',
 ];
 
