7af4b8d256f29806f92c67709d9df992e66e0df5
Vitaliy <theyamshikov@yandex.ru>
2018-08-11 22:20:12 +0600

    upgrade rbac

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -3,6 +3,9 @@
 namespace yii2module\article\admin;
 
 use yii\base\Module as YiiModule;
+use yii2lab\helpers\Behavior;
+use yii2module\article\domain\enums\ArticlePermissionEnum;
+
 
 /**
  * welcome module definition class
@@ -12,4 +15,10 @@ class Module extends YiiModule
 
 	public static $langDir = '@yii2module/article/domain/messages';
 
+    public function behaviors()
+    {
+        return [
+            'access' => Behavior::access(ArticlePermissionEnum::POST_MANAGE),
+        ];
+    }
 }
